6e888c433c8e9779892ce89c1e767ce28582129f
Christopher Duggan <christopherduggan@gmail.com>
2019-04-01 22:48:03 -0400

    made a new file to consolidate methods, updated d10 method, rawRoll, adjustRoll, checkBotch, checkCrit, attack, dodge, and successCheck

---
+++ game.js
@@ -0,0 +1,83 @@
+const game = {
+  attributes: {agility: 3},
+  abilities: {attack: 3, dodge: 3},
+
+  d10(dice) {
+    return this.adjustRoll(this.rawRoll(dice));
+  },
+  rawRoll(dice) {
+    let rawArr = [];
+    for (let i = 0; i < (dice || 1); i++) {
+      rawArr.push((Math.ceil(Math.random() * 10)))
+    }
+    console.log(`rawArr: [${rawArr}]`);
+    return rawArr;
+  },
+  adjustRoll(arr) {
+    arr.sort(function(a,b){
+        return a - b;
+      })
+    while(arr.includes(1) && arr.some((num) => num > 5)) {
+      arr.shift()
+      arr.pop()
+    }
+    console.log(`arr: [${arr}]`)
+    return arr;
+  },
+  checkBotch(arr) {
+    if (arr.includes(1)){
+      console.log('botch!')
+      return true;
+    } else {
+      return false;
+    }
+  },
+  checkCrit(arr) {
+    if (arr.includes(10)) {
+      console.log('it\'s a crit threat')
+      if (this.rawRoll() > 5) {
+        console.log('crit success!')
+        return true;
+      } else {
+        console.log('it didn\'t succeed though');
+        return false;
+      }
+    }
+  },
+  attack(target) {
+    let attackResult = this.d10(this.attributes.agility + this.abilities.attack);
+    this.checkBotch(attackResult);
+    if (this.checkCrit(attackResult)) {
+
+    };
+    return attackResult;
+  },
+
+// dodge must pass in attack result, do comparison inside. 
+  dodge() {
+    let dodgeResult = this.d10(this.attributes.agility + this.abilities.dodge);
+    this.checkBotch(dodgeResult);
+    return dodgeResult;
+  },
+  successCheck(offenseArr, defenseArr) {
+    let successCount = 0;
+    for(let i = 0; i < offenseArr.length; i++) {
+      if (offenseArr[i] > 5) {
+        successCount++;
+      }
+    }
+    for (let i = 0; i < defenseArr.length; i++) {
+      if(defenseArr[i] > 5) {
+        successCount--;
+      }
+    }
+    return successCount
+  },
+  hitCheck() {
+
+  }
+}
+
+game.attack(game)
+// game.dodge()
+// game.checkCrit(game.adjustRoll(game.rawRoll(5)))
